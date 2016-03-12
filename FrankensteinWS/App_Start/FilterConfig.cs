59ad8028c9dc34854209dcd07a7f0791d09a822c
malecsandra <alexandra.mindrean@yahoo.com>
2016-03-12 15:37:56 +0200

    initial commit

---
+++ FrankensteinWS/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FrankensteinWS
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ FrankensteinWS/Controllers/UsersController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FrankensteinWS.Controllers
+{
+    public class UsersController : ApiController
+    {
+        FrankensteinEntities db = new FrankensteinEntities();
+        // GET api/Users/5
+        public User Get(int id)
+        {
+            User userTest = db.Users.Include("Visits").Where(u => u.ID == id).FirstOrDefault();
+            /*List<Visit> visits = userTest.Visits.ToList();
+
+            List<User> users = db.Users.ToList();
+
+            string name = db.Users.Where(u => u.ID == 1).Select(u => u.NAME).FirstOrDefault();
+
+            List<Visit> visits2 = db.Visits.Where(v => v.ID_USER == userTest.ID).ToList();
+
+            List<GetUsersTest_Result> result = db.GetUsersTest(null).ToList();
+            */
+            return userTest;
+        }
+
+        public void Post(int id, [FromBody]User PostUser)
+        {
+            User user = db.Users.Where(u => u.ID == id).FirstOrDefault();
+            if (user == null)
+            {
+                //user nu exista in bd
+            }
+            else
+            {
+                user.NAME = PostUser.NAME;
+                user.PASSWORD = PostUser.PASSWORD;
+                db.SaveChanges();
+            }
+        }
+
+        public void Put([FromBody]User PostUser)
+        {
+            User NewUser = new User();
+            NewUser.NAME = PostUser.NAME;
+            NewUser.PASSWORD = PostUser.PASSWORD;
+
+            db.Users.Add(NewUser);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var user = new User { ID = 1 };
+            db.Users.Attach(user);
+            db.Users.Remove(user);
+            db.SaveChanges();
+        }
+    }
+}
